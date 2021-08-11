e6ac6779b69ced0f4eebdbf377e3f159bae7fe9c
Alec Muffett <alec.muffett@gmail.com>
2021-08-11 19:54:00 +0100

    make on 20210811195400

--- filter.pl
+++ filter.pl
@@ -85,7 +85,7 @@ foreach $key (@keys) {
 
     my $tweet_anchor = "$key";
     $tweet_anchor =~ s/\s/-/go;
-    my $tweet_text = "Check out the latest Twitter debate about '$key' with the #ReadyMadeTwitterSearch at $tweet_root#$tweet_anchor";
+    my $tweet_text = "Check out the latest Twitter debate about '$key' with the #ReadyMadeTwitterSearch at:\n\n$tweet_root#$tweet_anchor";
     my $tweet_url = sprintf("%s=%s", $tweet_intent, uri_escape($tweet_text));
     print "* [NEW: Tweet/Share this Search for '$key'!]($tweet_url) :heart:\n";
 
