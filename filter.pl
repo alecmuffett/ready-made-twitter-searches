de97ef3a610921c90895920d90c86e97bb0fc8a1
Alec Muffett <alec.muffett@gmail.com>
2022-02-02 01:33:16 +0000

    make on 20220202013316

--- filter.pl
+++ filter.pl
@@ -123,7 +123,7 @@ foreach $key (@keys) {
 
     my $tweet_anchor = $anchors{$key};
     my $tweet_key = join(' ', map {ucfirst} split(' ', $key));
-    my $tweet_text = "Debate continues! Check out the latest Twitter discussion regarding:\n\n$tweet_key\n\n\N{EM DASH} with a #ReadyMadeTwitterSearch at:\n\n$tweet_root#$tweet_anchor";
+    my $tweet_text = "Debate continues! Check out the latest Twitter discussion on:\n\n$tweet_key\n\n\N{EM DASH} with a #ReadyMadeTwitterSearch at:\n\n$tweet_root#$tweet_anchor";
     my $tweet_url = sprintf("%s=%s", $tweet_intent, uri_escape_utf8($tweet_text));
     print "* :heart: [Share this Search for '$key' in a Tweet!]($tweet_url)\n";
 
