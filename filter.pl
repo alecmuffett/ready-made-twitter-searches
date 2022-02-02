ef981bb8edc5f28162ff5e6747d072e7b99e3290
Alec Muffett <alec.muffett@gmail.com>
2022-02-02 01:48:03 +0000

    make on 20220202014803

--- filter.pl
+++ filter.pl
@@ -117,8 +117,7 @@ foreach $key (@keys) {
     $query_text =~ s/\s+/ /go;
     $query_text =~ s/\)\s\(/)(/go;
     my $query_length = length $query_text;
-    warn "overlong: $query_length query for $key\n" if ($query_length > $max_query_length);
-    #warn "len: $query_length: $key\n";
+    die "overlong: $query_length query for $key\n" if ($query_length > $max_query_length);
 
     my $latest = sprintf($search_latest, &Escape($query_text));
     print "* :point_right: [$key - LATEST Tweets]($latest)\n";
